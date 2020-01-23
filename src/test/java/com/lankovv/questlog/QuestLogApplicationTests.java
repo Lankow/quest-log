ec64cee58856b8a5891e030249af76c445f65726
Voknal <Maciej.Lankow@gmail.com>
2020-01-23 16:07:38 +0100

    Initial commit

---
+++ src/test/java/com/lankovv/questlog/QuestLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lankovv.questlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
